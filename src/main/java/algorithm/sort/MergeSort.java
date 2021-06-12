3efe1786ed036430b2ff7e7e62060672fce4fdb7
songyijia <author@mail.com>
2021-06-12 21:14:42 +0800

    归并排序

---
+++ src/main/java/algorithm/sort/MergeSort.java
@@ -0,0 +1,77 @@
+package algorithm.sort;
+
+/**
+ * 归并排序，每个部分排序后，将结果合并
+ */
+public class MergeSort {
+    public static void main(String[] args) {
+        int[] arr = {5,4,3,6,7,1,2,7,6,8};
+        process2(arr,0,arr.length-1);
+        for (int i : arr) {
+            System.out.print(i+"    ");
+        }
+    }
+
+    public static void process(int[] arr,int left,int right){
+        if (left==right){
+            return;
+        }
+        int mid = left + ((right - left)>>1);
+        process(arr,left,mid);
+        process(arr,mid+1,right);
+        merge(arr,left,mid,right);
+    }
+
+    /**
+     * 非递归实现
+     * @param arr
+     * @param left
+     * @param right
+     */
+    public static void process2(int[] arr,int left,int right){
+        if (arr == null || left==right){
+            return;
+        }
+        int N = arr.length;
+        int mergeSize = 1;
+        while(mergeSize < N){
+            int L = 0;
+            while (L < N){
+                int M = L + mergeSize -1;
+                if (M >= N) {
+                    break;
+                }
+                int R = Math.min(M+mergeSize,N-1);
+                merge(arr,L,M,R);
+                L = R+1;
+            }
+            if (mergeSize > N /2){
+                break;
+            }
+            mergeSize <<= 1;
+
+        }
+    }
+
+    private static void merge(int[] arr, int left, int mid, int right) {
+        int[] help = new int[right-left+1];
+        int i=0;int l = left; int r = mid+1;
+        while (l<=mid && r <= right){
+            //左右数组比较，将两边小的赋值给新数组
+            help[i++] = arr[l]<=arr[r]?arr[l++]:arr[r++];
+        }
+        //将两边数组剩余元素复制到新数组
+        while (l<=mid){
+            help[i++] = arr[l++];
+        }
+        while (r<=right){
+            help[i++] = arr[r++];
+        }
+        //将help数组重写回原数组
+//        arr = help;  这样传不过去
+        //注意这里的复制是归并的
+        for (int j = 0; j < help.length; j++) {
+            arr[left+j]= help[j];
+        }
+    }
+}
